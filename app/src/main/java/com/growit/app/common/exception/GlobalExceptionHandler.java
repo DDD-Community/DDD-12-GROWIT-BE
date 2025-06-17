27e002860a8e2d76918f11b971e35d0b3d47da53
LJS107 <juneseok0107@naver.com>
2025-06-17 10:30:57 +0900

    fix(interceptor): return message Exception 메시지로 변경
    Change-Id: I4a6970252d33a18940f943d84473b886caa6b3f8

--- app/src/main/java/com/growit/app/common/exception/GlobalExceptionHandler.java
+++ app/src/main/java/com/growit/app/common/exception/GlobalExceptionHandler.java
@@ -21,6 +21,6 @@ public class GlobalExceptionHandler {
     // 알 수 없는 에러는 500으로 처리
     return ResponseEntity
       .status(500)
-      .body(new SimpleErrorResponse(500, "서버 오류가 발생했습니다."));
+      .body(new SimpleErrorResponse(500, ex.getMessage()));
   }
 }
