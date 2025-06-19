c6b17c840923ef8ce97a1ba9f6297f780e088f88
LJS107 <juneseok0107@naver.com>
2025-06-19 15:57:30 +0900

    feat(User): 도메인(회원)과 엔티티(회원) 생성 및 기능 구현
    Change-Id: I2799b569cee29d70f9a5cf6f35cf348260b4ec79

--- app/src/main/java/com/growit/app/user/domain/user/dto/SignUpCommand.java
+++ app/src/main/java/com/growit/app/user/domain/user/dto/SignUpCommand.java
@@ -1,3 +1,13 @@
 package com.growit.app.user.domain.user.dto;
 
-public record SignUpCommand() {}
+import com.growit.app.user.domain.user.vo.CareerYear;
+import com.growit.app.user.domain.user.vo.Email;
+
+public record SignUpCommand(
+  Email email,
+  String password,
+  String name,
+  String jobRoleId,
+  CareerYear careerYear
+) {
+}
