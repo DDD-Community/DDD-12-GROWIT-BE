c354ab1dc00a9338f5d7dba4e01235f49ea27e6d
LJS107 <juneseok0107@naver.com>
2025-06-19 09:42:29 +0900

    style(comment): 불필요한 주석제거
    Change-Id: Ia8ac553c3fe4e52901e79f840e4fbeff8be558bc

--- app/src/main/java/com/growit/app/common/entity/BaseEntity.java
+++ app/src/main/java/com/growit/app/common/entity/BaseEntity.java
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
-  protected BaseEntity() {} // JPA를 위한 기본 생성자
+  protected BaseEntity() {}
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
