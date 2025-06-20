c2709eaa86e310a3c7f82d36218f3fb313ac1f50
JSL107 <juneseok0107@naver.com>
2025-06-21 02:02:49 +0900

    fix(deploy): 배포용 설정으로 변경
    h2 database => postgres
    
    Change-Id: I657ee6ec41191eec0a422ef3e6cab9395ca470a2

--- app/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO jobrole (uid, name, created_at, updated_at)
-VALUES ('uid1', '개발자', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO jobrole (uid, name, created_at, updated_at)
-VALUES ('uid2', '디자이너', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO jobrole (uid, name, created_at, updated_at)
-VALUES ('uid3', '기획자', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
