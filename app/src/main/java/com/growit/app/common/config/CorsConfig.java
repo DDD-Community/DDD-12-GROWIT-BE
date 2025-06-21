b556396e359d27867d0e5e2fd27e87e06fe0f008
dyno-jun <dyno.jun@gmail.com>
2025-06-22 07:34:45 +0900

    feat(cors): cors 허용 적용
    - * 을 통해 전체 cors 적용
    
    Closes #19
    
    [skip ci]
    
    Change-Id: I611d358f918b760b7e3ff22e63a4b916b50d0a96

--- app/src/main/java/com/growit/app/common/config/CorsConfig.java
+++ app/src/main/java/com/growit/app/common/config/CorsConfig.java
@@ -10,11 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry
-      .addMapping("/**") // 모든 API 경로에 대해
-      .allowedOrigins("*") // 허용할 프론트 주소, 우선 전체 허용
-      .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 메서드
-      .allowedHeaders("*") // 모든 헤더 허용
-      .allowCredentials(false) // 인증 정보(Cookie 등) 허용
-      .maxAge(3600); // preflight 요청 결과를 캐시하는 시간(초)
+        .addMapping("/**") // 모든 API 경로에 대해
+        .allowedOrigins("*") // 허용할 프론트 주소, 우선 전체 허용
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 메서드
+        .allowedHeaders("*") // 모든 헤더 허용
+        .allowCredentials(false) // 인증 정보(Cookie 등) 허용
+        .maxAge(3600); // preflight 요청 결과를 캐시하는 시간(초)
   }
 }
