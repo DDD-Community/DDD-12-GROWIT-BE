4e8516e1091f3fdd0350e031f81834b07304cc3d
LJS107 <juneseok0107@naver.com>
2025-06-24 18:01:55 +0900

    test: UserTestBuilder, GoalTestBuilder 생성
    Change-Id: I6c7e739515532727df32acfbdf2e29d724d030e4

--- app/src/test/java/com/growit/app/utils/fixture/GoalTestBuilder.java
+++ app/src/test/java/com/growit/app/utils/fixture/GoalTestBuilder.java
@@ -4,7 +4,6 @@ import com.growit.app.goal.domain.goal.Goal;
 import com.growit.app.goal.domain.goal.plan.Plan;
 import com.growit.app.goal.domain.goal.vo.BeforeAfter;
 import com.growit.app.goal.domain.goal.vo.GoalDuration;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -17,12 +16,35 @@ public class GoalTestBuilder {
   private BeforeAfter beforeAfter = null; // 필요하다면 기본값 생성
   private List<Plan> plans = new ArrayList<>();
 
-  public GoalTestBuilder id(String id) { this.id = id; return this; }
-  public GoalTestBuilder userId(String userId) { this.userId = userId; return this; }
-  public GoalTestBuilder name(String name) { this.name = name; return this; }
-  public GoalTestBuilder duration(GoalDuration duration) { this.duration = duration; return this; }
-  public GoalTestBuilder beforeAfter(BeforeAfter beforeAfter) { this.beforeAfter = beforeAfter; return this; }
-  public GoalTestBuilder plans(List<Plan> plans) { this.plans = plans; return this; }
+  public GoalTestBuilder id(String id) {
+    this.id = id;
+    return this;
+  }
+
+  public GoalTestBuilder userId(String userId) {
+    this.userId = userId;
+    return this;
+  }
+
+  public GoalTestBuilder name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public GoalTestBuilder duration(GoalDuration duration) {
+    this.duration = duration;
+    return this;
+  }
+
+  public GoalTestBuilder beforeAfter(BeforeAfter beforeAfter) {
+    this.beforeAfter = beforeAfter;
+    return this;
+  }
+
+  public GoalTestBuilder plans(List<Plan> plans) {
+    this.plans = plans;
+    return this;
+  }
 
   public Goal build() {
     return new Goal(id, userId, name, duration, beforeAfter, plans);
--- app/src/test/java/com/growit/app/utils/fixture/UserTestBuilder.java
+++ app/src/test/java/com/growit/app/utils/fixture/UserTestBuilder.java
@@ -14,13 +14,40 @@ public class UserTestBuilder {
   private CareerYear careerYear = CareerYear.NEWBIE;
   private RequiredConsent requiredConsent = requiredConsent().build().getRequiredConsent();
 
-  public UserTestBuilder id(String id) { this.id = id; return this; }
-  public UserTestBuilder email(Email email) { this.email = email; return this; }
-  public UserTestBuilder password(String password) { this.password = password; return this; }
-  public UserTestBuilder name(String name) { this.name = name; return this; }
-  public UserTestBuilder jobRoleId(String jobRoleId) { this.jobRoleId = jobRoleId; return this; }
-  public UserTestBuilder careerYear(CareerYear careerYear) { this.careerYear = careerYear; return this; }
-  public UserTestBuilder requiredConsent(RequiredConsent requiredConsent) { this.requiredConsent = requiredConsent; return this; }
+  public UserTestBuilder id(String id) {
+    this.id = id;
+    return this;
+  }
+
+  public UserTestBuilder email(Email email) {
+    this.email = email;
+    return this;
+  }
+
+  public UserTestBuilder password(String password) {
+    this.password = password;
+    return this;
+  }
+
+  public UserTestBuilder name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public UserTestBuilder jobRoleId(String jobRoleId) {
+    this.jobRoleId = jobRoleId;
+    return this;
+  }
+
+  public UserTestBuilder careerYear(CareerYear careerYear) {
+    this.careerYear = careerYear;
+    return this;
+  }
+
+  public UserTestBuilder requiredConsent(RequiredConsent requiredConsent) {
+    this.requiredConsent = requiredConsent;
+    return this;
+  }
 
   public User build() {
     return new User(id, email, password, name, jobRoleId, careerYear, requiredConsent);
