4ac7bebec29abdb23e6b38caf3ca14acc1ff0be4
JSL107 <juneseok0107@naver.com>
2025-06-25 23:23:02 +0900

    feat(Fixture): Goal Fixture, Bulder 추가
    테스트에서 사용하기 위한 데이터 추가

--- app/src/test/java/com/growit/app/fake/goal/GoalFixture.java
+++ app/src/test/java/com/growit/app/fake/goal/GoalFixture.java
@@ -1,23 +1,26 @@
-package com.growit.app.utils.fixture;
+package com.growit.app.fake.goal;
 
 import com.growit.app.goal.domain.goal.Goal;
 import com.growit.app.goal.domain.goal.plan.Plan;
 import com.growit.app.goal.domain.goal.vo.BeforeAfter;
 import com.growit.app.goal.domain.goal.vo.GoalDuration;
-import groovy.transform.builder.Builder;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
 
-@Builder
-public class GoalTestBuilder {
-  LocalDate today = LocalDate.now();
+public class GoalFixture {
+  public static Goal defaultGoal() {
+    return new GoalBuilder().build();
+  }
+}
 
+class GoalBuilder {
+  LocalDate today = LocalDate.now();
   LocalDate thisMonday = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
   LocalDate thisSunday = today.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
-
   String asIs = "ASIS";
   String toBe = "TOBE";
 
@@ -28,41 +31,45 @@ public class GoalTestBuilder {
   private BeforeAfter beforeAfter = new BeforeAfter(asIs, toBe);
   private List<Plan> plans = new ArrayList<>();
 
-  public static GoalTestBuilder aGoal() {
-    return new GoalTestBuilder();
-  }
-
-  public GoalTestBuilder id(String id) {
+  public GoalBuilder id(String id) {
     this.id = id;
     return this;
   }
 
-  public GoalTestBuilder userId(String userId) {
+  public GoalBuilder userId(String userId) {
     this.userId = userId;
     return this;
   }
 
-  public GoalTestBuilder name(String name) {
+  public GoalBuilder name(String name) {
     this.name = name;
     return this;
   }
 
-  public GoalTestBuilder duration(GoalDuration duration) {
+  public GoalBuilder duration(GoalDuration duration) {
     this.duration = duration;
     return this;
   }
 
-  public GoalTestBuilder beforeAfter(BeforeAfter beforeAfter) {
+  public GoalBuilder beforeAfter(BeforeAfter beforeAfter) {
     this.beforeAfter = beforeAfter;
     return this;
   }
 
-  public GoalTestBuilder plans(List<Plan> plans) {
+  public GoalBuilder plans(List<Plan> plans) {
     this.plans = plans;
     return this;
   }
 
   public Goal build() {
-    return new Goal(id, userId, name, duration, beforeAfter, plans);
+    return Goal.builder()
+      .id(id)
+      .userId(userId)
+      .name(name)
+      .duration(duration)
+      .beforeAfter(beforeAfter)
+      .plans(plans)
+      .build();
   }
+
 }
