b967814ca29a25b1d29cb018cae08af49dc959c5
JSL107 <juneseok0107@naver.com>
2025-06-27 00:00:17 +0900

    feat: GoalService
    checkMyGoal 기능 구현
    - 내 기능만 삭제할 수 있다

--- app/src/main/java/com/growit/app/goal/usecase/DeleteGoalUseCase.java
+++ app/src/main/java/com/growit/app/goal/usecase/DeleteGoalUseCase.java
@@ -1,10 +1,10 @@
 package com.growit.app.goal.usecase;
 
-import com.growit.app.common.exception.BadRequestException;
 import com.growit.app.common.exception.NotFoundException;
 import com.growit.app.goal.domain.goal.Goal;
 import com.growit.app.goal.domain.goal.GoalRepository;
 import com.growit.app.goal.domain.goal.dto.DeleteGoalCommand;
+import com.growit.app.goal.domain.goal.service.GoalValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,17 +13,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DeleteGoalUseCase {
   private final GoalRepository goalRepository;
+  private final GoalValidator goalValidator;
 
   @Transactional
   public void execute(DeleteGoalCommand command) {
     Goal goal =
         goalRepository
-            .findByUid(command.uid())
+            .findById(command.id())
             .orElseThrow(() -> new NotFoundException("해당 되는 목표가 없습니다."));
 
-    if (!goal.getUserId().equals(command.userId())) {
-      throw new BadRequestException("삭제 권한이 없습니다.");
-    }
+    goalValidator.checkMyGoal(goal, command.userId());
+
     goal.markAsDeleted();
     goalRepository.saveGoal(goal);
   }
