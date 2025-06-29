f5e39bed2896eb8c1ee4efa0228f61f19fd75c8b
JSL107 <juneseok0107@naver.com>
2025-06-29 12:58:40 +0900

    docs: test 완료

--- app/src/main/java/com/growit/app/common/config/CorsConfig.java
+++ app/src/main/java/com/growit/app/common/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
         .allowedOrigins("*") // 허용할 프론트 주소, 우선 전체 허용
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 메서드
         .allowedHeaders("*") // 모든 헤더 허용
-        .allowCredentials(true) // 인증 정보(Cookie 등) 허용
+        .allowCredentials(false) // 인증 정보(Cookie 등) 허용
         .maxAge(3600); // preflight 요청 결과를 캐시하는 시간(초)
   }
 }
