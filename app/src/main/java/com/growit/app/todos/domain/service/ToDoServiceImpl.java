d8abfe4971cd3681681ebc95214aaa5839c6a76b
LJS107 <juneseok0107@naver.com>
2025-07-01 17:20:31 +0900

    docs: throw message 문구 변경
    Change-Id: I919a1936fb721610402bbe5dc56f5e6d065d2802

--- app/src/main/java/com/growit/app/todos/domain/service/ToDoServiceImpl.java
+++ app/src/main/java/com/growit/app/todos/domain/service/ToDoServiceImpl.java
@@ -15,7 +15,7 @@ public class ToDoServiceImpl implements ToDoService {
         (date.isEqual(lastWeekMonday) || date.isAfter(lastWeekMonday))
             && (date.isEqual(thisWeekSunday) || date.isBefore(thisWeekSunday));
     if (!inRange) {
-      throw new IllegalArgumentException("ToDo는 지난 주 월요일부터 이번 주 일요일까지만 생성할 수 있습니다.");
+      throw new IllegalArgumentException("ToDo는 지난 주, 이번 주만 생성할 수 있습니다.");
     }
   }
 }
