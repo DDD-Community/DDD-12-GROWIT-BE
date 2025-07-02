ea7424eea2f8736c2acd0c95dbd9d382d972bc0d
LJS107 <juneseok0107@naver.com>
2025-07-02 14:09:34 +0900

    fix: exception 누락 추가
    Change-Id: I05272a7ab30cb503febdf1897bc31d04ce785367

--- app/src/main/java/com/growit/app/todos/domain/service/ToDoService.java
+++ app/src/main/java/com/growit/app/todos/domain/service/ToDoService.java
@@ -12,7 +12,7 @@ public class ToDoService implements ToDoValidator {
   private final ToDoRepository toDoRepository;
 
   @Override
-  public void isDateInRange(LocalDate date) {
+  public void isDateInRange(LocalDate date) throws BadRequestException {
     LocalDate today = LocalDate.now();
     LocalDate lastWeekMonday = today.minusWeeks(1).with(java.time.DayOfWeek.MONDAY);
     LocalDate thisWeekSunday = today.with(java.time.DayOfWeek.SUNDAY);
@@ -22,14 +22,14 @@ public class ToDoService implements ToDoValidator {
     }
   }
 
-  public void tooManyToDoCreated(LocalDate date, String userId) {
+  public void tooManyToDoCreated(LocalDate date, String userId) throws BadRequestException {
     int count = toDoRepository.countByToDo(date, userId);
     if (count >= 10) {
       throw new BadRequestException("하루에 할 일은 최대 10개까지만 등록할 수 있습니다.");
     }
   }
 
-  public void checkContent(String content) {
+  public void checkContent(String content) throws BadRequestException {
     if (content == null || content.length() < 5 || content.length() >= 30) {
       throw new IllegalArgumentException("내용은 5자 이상 30자 미만이어야 합니다.");
     }
