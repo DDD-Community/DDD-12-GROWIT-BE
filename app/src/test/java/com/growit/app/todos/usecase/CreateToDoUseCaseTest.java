c8eb7a5f238c06285d2d179b478f8e9c93d3b22f
LJS107 <juneseok0107@naver.com>
2025-07-02 14:14:20 +0900

    feat: usecase test 구현
    Change-Id: If1bd13d17bbd1a70274f13e134789d17adf67790

--- app/src/test/java/com/growit/app/todos/usecase/CreateToDoUseCaseTest.java
+++ app/src/test/java/com/growit/app/todos/usecase/CreateToDoUseCaseTest.java
@@ -1,3 +1,45 @@
 package com.growit.app.todos.usecase;
 
-public class CreateToDoUseCaseTest {}
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.growit.app.fake.todos.FakeToDoRepository;
+import com.growit.app.fake.todos.ToDoFixture;
+import com.growit.app.todos.controller.dto.CreateToDoRequest;
+import com.growit.app.todos.domain.CreateToDoCommand;
+import com.growit.app.todos.domain.service.ToDoService;
+import com.growit.app.todos.domain.service.ToDoValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CreateToDoUseCaseTest {
+
+  private CreateToDoUseCase createToDoUseCase;
+  private FakeToDoRepository fakeToDoRepository;
+
+  @BeforeEach
+  void setUp() {
+    fakeToDoRepository = new FakeToDoRepository();
+    ToDoValidator toDoValidator = new ToDoService(fakeToDoRepository);
+    createToDoUseCase = new CreateToDoUseCase(toDoValidator, fakeToDoRepository);
+  }
+
+  @Test
+  void givenValidCommand_whenExecute_thenReturnsToDoId() {
+    // Given
+    CreateToDoRequest request = ToDoFixture.defaultCreateToDoRequest();
+
+    CreateToDoCommand command =
+        new CreateToDoCommand(
+            request.getGoalId(),
+            request.getPlanId(),
+            "user-1",
+            request.getContent(),
+            request.getDate());
+
+    // When
+    String toDoId = createToDoUseCase.execute(command);
+
+    // Then
+    assertNotNull(toDoId);
+  }
+}
