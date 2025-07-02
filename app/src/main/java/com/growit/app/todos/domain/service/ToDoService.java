9cbdd39ce4df00c49adf4c6ec2982c9c18df559e
JSL107 <juneseok0107@naver.com>
2025-07-02 23:32:37 +0900

    fix: checkContent 미사용

--- app/src/main/java/com/growit/app/todos/domain/service/ToDoService.java
+++ app/src/main/java/com/growit/app/todos/domain/service/ToDoService.java
@@ -1,8 +1,11 @@
 package com.growit.app.todos.domain.service;
 
 import com.growit.app.common.exception.BadRequestException;
+import com.growit.app.goal.domain.goal.Goal;
+import com.growit.app.goal.domain.goal.GoalRepository;
 import com.growit.app.todos.domain.ToDoRepository;
 import java.time.LocalDate;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +13,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ToDoService implements ToDoValidator {
   private final ToDoRepository toDoRepository;
-
+  private final GoalRepository goalRepository;
+  private final static int MAX_TO_COUNT = 10;
   @Override
-  public void isDateInRange(LocalDate date) throws BadRequestException {
+  public void isDateInRange(LocalDate date, String goalId) throws BadRequestException {
     LocalDate today = LocalDate.now();
-    LocalDate lastWeekMonday = today.minusWeeks(1).with(java.time.DayOfWeek.MONDAY);
+
+    Goal goalEntity = goalRepository.findById(goalId)
+      .orElseThrow(() -> new BadRequestException("해당 목표(goal)가 존재하지 않습니다."));
+    LocalDate thisWeekStartDate = goalEntity.getDuration().startDate();
+
     LocalDate thisWeekSunday = today.with(java.time.DayOfWeek.SUNDAY);
 
-    if (date.isBefore(lastWeekMonday) || date.isAfter(thisWeekSunday)) {
+    if (date.isBefore(thisWeekStartDate) || date.isAfter(thisWeekSunday)) {
       throw new BadRequestException("ToDo는 지난 주, 이번 주만 생성할 수 있습니다.");
     }
   }
@@ -25,14 +33,8 @@ public class ToDoService implements ToDoValidator {
   public void tooManyToDoCreated(LocalDate date, String userId, String planId)
       throws BadRequestException {
     int count = toDoRepository.countByToDo(date, userId, planId);
-    if (count >= 10) {
+    if (count >= MAX_TO_COUNT) {
       throw new BadRequestException("하루에 할 일은 최대 10개까지만 등록할 수 있습니다.");
-    }
-  }
-
-  public void checkContent(String content) throws BadRequestException {
-    if (content == null || content.length() < 5 || content.length() >= 30) {
-      throw new BadRequestException("내용은 5자 이상 30자 미만이어야 합니다.");
     }
   }
 }
