c41c1c940b882e0c1cc11b7e4383137e8163d191
LJS107 <juneseok0107@naver.com>
2025-07-07 17:42:00 +0900

    fix: WeeklyPlan QueryDSL 방식으로 변경
    Change-Id: I77bb2f733fadb1489634e601199046992f3a1576

--- app/src/main/java/com/growit/app/todo/infrastructure/persistence/todos/source/DBToDoQueryRepository.java
+++ app/src/main/java/com/growit/app/todo/infrastructure/persistence/todos/source/DBToDoQueryRepository.java
@@ -1,8 +1,12 @@
 package com.growit.app.todo.infrastructure.persistence.todos.source;
 
 import com.growit.app.todo.infrastructure.persistence.todos.source.entity.ToDoEntity;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface DBToDoQueryRepository {
   Optional<ToDoEntity> findByUid(String uid);
+
+  List<ToDoEntity> findByUserIdAndGoalIdAndPlanId(String userId, String goalId, String planId);
 }
--- app/src/main/java/com/growit/app/todo/infrastructure/persistence/todos/source/DBToDoQueryRepositoryImpl.java
+++ app/src/main/java/com/growit/app/todo/infrastructure/persistence/todos/source/DBToDoQueryRepositoryImpl.java
@@ -2,9 +2,13 @@ package com.growit.app.todo.infrastructure.persistence.todos.source;
 
 import static com.growit.app.todo.infrastructure.persistence.todos.source.entity.QToDoEntity.toDoEntity;
 
+import com.growit.app.todo.infrastructure.persistence.todos.source.entity.QToDoEntity;
 import com.growit.app.todo.infrastructure.persistence.todos.source.entity.ToDoEntity;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import java.util.List;
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -17,9 +21,23 @@ public class DBToDoQueryRepositoryImpl implements DBToDoQueryRepository {
   @Override
   public Optional<ToDoEntity> findByUid(String uid) {
     return Optional.ofNullable(
-        queryFactory
-            .selectFrom(toDoEntity)
-            .where(toDoEntity.uid.eq(uid), toDoEntity.deletedAt.isNull())
-            .fetchOne());
+      queryFactory
+        .selectFrom(toDoEntity)
+        .where(toDoEntity.uid.eq(uid), toDoEntity.deletedAt.isNull())
+        .fetchOne());
+  }
+
+  @Override
+  public List<ToDoEntity> findByUserIdAndGoalIdAndPlanId(String userId, String goalId, String planId) {
+    QToDoEntity toDo = QToDoEntity.toDoEntity;
+    return queryFactory
+      .selectFrom(toDo)
+      .where(
+        toDo.userId.eq(userId),
+        toDo.goalId.eq(goalId),
+        toDo.planId.eq(planId),
+        toDo.deletedAt.isNull()
+      )
+      .fetch();
   }
 }
