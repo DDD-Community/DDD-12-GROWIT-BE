755fb031b55ba63586b47737454bb50d1426a0ba
LJS107 <juneseok0107@naver.com>
2025-07-08 17:12:57 +0900

    test: isDelete가 false인 todo 찾을수 있게 구현
    Change-Id: I9922391d028fc8abb19358a604e6f011ef699c98

--- app/src/main/java/com/growit/app/todo/domain/service/ToDoService.java
+++ app/src/main/java/com/growit/app/todo/domain/service/ToDoService.java
@@ -59,7 +59,7 @@ public class ToDoService implements ToDoValidator, ToDoQuery {
       throw new NotFoundException("사용자 정보가 일치하지 않습니다.");
     }
 
-    if(todo.isDeleted()) {
+    if (todo.isDeleted()) {
       throw new NotFoundException("존재하지 않는 ToDo 입니다.");
     }
 
