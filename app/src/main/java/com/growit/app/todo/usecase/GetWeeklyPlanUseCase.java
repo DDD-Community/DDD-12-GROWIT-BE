e2c8ed6e93fd8a23aa6409a08075b6840e322e9b
LJS107 <juneseok0107@naver.com>
2025-07-09 15:51:18 +0900

    fix: 1차 pr fix
    Change-Id: I92acc12def3cba7e3be43a96e4310a1b6eb3bee0

--- app/src/main/java/com/growit/app/todo/usecase/GetWeeklyPlanUseCase.java
+++ app/src/main/java/com/growit/app/todo/usecase/GetWeeklyPlanUseCase.java
@@ -7,13 +7,11 @@ import com.growit.app.goal.domain.goal.service.GoalService;
 import com.growit.app.todo.domain.ToDo;
 import com.growit.app.todo.domain.ToDoRepository;
 import jakarta.transaction.Transactional;
-
 import java.time.DayOfWeek;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,15 +25,18 @@ public class GetWeeklyPlanUseCase {
   @Transactional
   public Map<DayOfWeek, List<ToDo>> execute(String goalId, String planId, String userId) {
     Goal goal =
-      goalRepository.findById(goalId).orElseThrow(() -> new NotFoundException("목표를 찾을 수 없습니다."));
+        goalRepository.findById(goalId).orElseThrow(() -> new NotFoundException("목표를 찾을 수 없습니다."));
     goalService.checkPlanExists(userId, goal.getId(), planId);
 
     List<ToDo> todos = toDoRepository.findByPlanId(planId);
 
-    Map<DayOfWeek, List<ToDo>> map = todos.stream()
-      .collect(
-        Collectors.groupingBy(
-          todo -> todo.getDate().getDayOfWeek(), LinkedHashMap::new, Collectors.toList()));
+    Map<DayOfWeek, List<ToDo>> map =
+        todos.stream()
+            .collect(
+                Collectors.groupingBy(
+                    todo -> todo.getDate().getDayOfWeek(),
+                    LinkedHashMap::new,
+                    Collectors.toList()));
 
     for (DayOfWeek day : DayOfWeek.values()) {
       map.putIfAbsent(day, List.of());
