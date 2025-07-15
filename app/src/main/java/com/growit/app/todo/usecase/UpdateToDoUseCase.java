5862a97b6873aa2087120fca619d5ad1238c431d
LJS107 <juneseok0107@naver.com>
2025-07-15 10:13:21 +0900

    fix : 미사용 주석 커밋
    Change-Id: Idd2e82dd11f1682d1ebe3d3ad565f706e392bd2e

--- app/src/main/java/com/growit/app/todo/usecase/UpdateToDoUseCase.java
+++ app/src/main/java/com/growit/app/todo/usecase/UpdateToDoUseCase.java
@@ -20,15 +20,12 @@ public class UpdateToDoUseCase {
   private final ToDoRepository toDoRepository;
   private final GoalQuery goalQuery;
 
-  //  private final ConventionCalculator conventionCalculator;
-
   @Transactional
   public void execute(UpdateToDoCommand command) {
     ToDo toDo = toDoQuery.getMyToDo(command.id(), command.userId());
     Goal goal = goalQuery.getMyGoal(toDo.getGoalId(), command.userId());
     Plan plan = goal.getPlanByDate(command.date());
 
-    //    conventionCalculator.isDateInRange(command.date());
     toDoValidator.tooManyToDoUpdated(command.date(), command.userId(), plan.getId(), toDo.getId());
 
     toDo.updateBy(command, plan.getId());
