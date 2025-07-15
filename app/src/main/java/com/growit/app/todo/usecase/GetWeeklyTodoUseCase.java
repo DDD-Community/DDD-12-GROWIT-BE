858815860ab8b5d3e946df02b2370eaaaf9fa7ca
LJS107 <juneseok0107@naver.com>
2025-07-15 12:54:17 +0900

    fix: 구현체 => 인터페이스 주입으로 변경
    Change-Id: I32ef9caf5dfad6005696fc2cdd8f80878991b6fb

--- app/src/main/java/com/growit/app/todo/usecase/GetWeeklyTodoUseCase.java
+++ app/src/main/java/com/growit/app/todo/usecase/GetWeeklyTodoUseCase.java
@@ -3,7 +3,7 @@ package com.growit.app.todo.usecase;
 import com.growit.app.common.exception.NotFoundException;
 import com.growit.app.goal.domain.goal.Goal;
 import com.growit.app.goal.domain.goal.GoalRepository;
-import com.growit.app.goal.domain.goal.service.GoalService;
+import com.growit.app.goal.domain.goal.service.GoalValidator;
 import com.growit.app.todo.domain.ToDo;
 import com.growit.app.todo.domain.ToDoRepository;
 import com.growit.app.todo.domain.util.ToDoUtils;
@@ -18,14 +18,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class GetWeeklyTodoUseCase {
   private final ToDoRepository toDoRepository;
-  private final GoalService goalService;
+  private final GoalValidator goalValidator;
   private final GoalRepository goalRepository;
 
   @Transactional(readOnly = true)
   public Map<DayOfWeek, List<ToDo>> execute(String goalId, String planId, String userId) {
     Goal goal =
         goalRepository.findById(goalId).orElseThrow(() -> new NotFoundException("목표를 찾을 수 없습니다."));
-    goalService.checkPlanExists(userId, goal.getId(), planId);
+    goalValidator.checkPlanExists(userId, goal.getId(), planId);
 
     List<ToDo> todos = toDoRepository.findByPlanId(planId);
     return ToDoUtils.groupByDayOfWeek(todos);
