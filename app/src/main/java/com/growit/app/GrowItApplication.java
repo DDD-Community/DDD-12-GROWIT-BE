40cadf8aa95c30ca41ce8ad193f0cd16970f7f05
JSL107 <juneseok0107@naver.com>
2025-07-20 19:17:47 +0900

    feat: caffeine cahce + spring boot 적용 완료

--- app/src/main/java/com/growit/app/GrowItApplication.java
+++ app/src/main/java/com/growit/app/GrowItApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableCaching
 public class GrowItApplication {
   public static void main(String[] args) {
     SpringApplication.run(GrowItApplication.class, args);
--- app/src/main/java/com/growit/app/goal/usecase/GetUserGoalsUseCase.java
+++ app/src/main/java/com/growit/app/goal/usecase/GetUserGoalsUseCase.java
@@ -3,19 +3,26 @@ package com.growit.app.goal.usecase;
 import com.growit.app.goal.domain.goal.Goal;
 import com.growit.app.goal.domain.goal.GoalRepository;
 import com.growit.app.user.domain.user.User;
-import java.util.Collections;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GetUserGoalsUseCase {
   private final GoalRepository goalRepository;
 
+  @Cacheable(cacheNames = "goalCache", value = "goalCache", key = "#user.id")
   @Transactional(readOnly = true)
   public List<Goal> getMyGoals(User user) {
+    log.info("Cache Miss!");
+
     List<Goal> goals = goalRepository.findAllByUserId(user.getId());
     if (goals.isEmpty()) return Collections.emptyList();
 
