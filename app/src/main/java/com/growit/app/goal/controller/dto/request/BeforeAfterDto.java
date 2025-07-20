e39f361ef5be1c10624ba742cfa7d816e8ee61e6
dyno-jun <dyno.jun@gmail.com>
2025-07-20 22:47:16 +0900

    feat(goal): 목표 상태도 20자
    Closes #156

--- app/src/main/java/com/growit/app/goal/controller/dto/request/BeforeAfterDto.java
+++ app/src/main/java/com/growit/app/goal/controller/dto/request/BeforeAfterDto.java
@@ -9,10 +9,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public class BeforeAfterDto {
   @NotBlank(message = "{validation.goal.asis.required}")
-  @Size(min = 1, max = 20, message = "{validation.goal.asis.size}")
+  @Size(min = 1, max = 30, message = "{validation.goal.asis.size}")
   private String asIs;
 
   @NotBlank(message = "{validation.goal.tobe.required}")
-  @Size(min = 1, max = 20, message = "{validation.goal.tobe.size}")
+  @Size(min = 1, max = 30, message = "{validation.goal.tobe.size}")
   private String toBe;
 }
