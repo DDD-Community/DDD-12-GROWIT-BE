807a79aa6d3be480fc95fa762795c7633e0e814d
LJS107 <juneseok0107@naver.com>
2025-08-05 09:38:04 +0900

    fix: 시작일 제약 조건 변경
    AS-IS
    - 시작일은 월요일부터만 생성이 가능하다.
    
    TO-BE
    - 시작일은 이번주부터 생성이 가능하다.
    
    Change-Id: I5b6b453f0e3534b52196f6c7079f5dc2989c5d4f

--- app/src/main/java/com/growit/app/goal/domain/goal/service/GoalService.java
+++ app/src/main/java/com/growit/app/goal/domain/goal/service/GoalService.java
@@ -21,19 +21,19 @@ import org.springframework.stereotype.Service;
 public class GoalService implements GoalValidator, GoalQuery {
   private final GoalRepository goalRepository;
 
+  // 시작 일은 이번주 부터 설정 가능으로 변경 (전주는 설정 불가)
   @Override
   public void checkGoalDuration(GoalDuration duration) {
-    if (duration.startDate().getDayOfWeek() != DayOfWeek.MONDAY) {
-      throw new BadRequestException(GOAL_DURATION_MONDAY.getCode());
-    }
-    if (duration.endDate().getDayOfWeek() != DayOfWeek.SUNDAY) {
-      throw new BadRequestException(GOAL_DURATION_SUNDAY.getCode());
-    }
-    if (!duration.endDate().isAfter(duration.startDate())) {
-      throw new BadRequestException(GOAL_DURATION_START_END.getCode());
-    }
-    if (duration.startDate().isBefore(LocalDate.now())) {
+    LocalDate startDate = duration.startDate();
+    LocalDate endDate = duration.endDate();
+    LocalDate today = LocalDate.now().with(DayOfWeek.MONDAY);
+
+    if (startDate.isBefore(today)) {
       throw new BadRequestException(GOAL_DURATION_START_AFTER_TODAY.getCode());
+    }
+
+    if (!endDate.isAfter(startDate)) {
+      throw new BadRequestException(GOAL_DURATION_START_END.getCode());
     }
   }
 
