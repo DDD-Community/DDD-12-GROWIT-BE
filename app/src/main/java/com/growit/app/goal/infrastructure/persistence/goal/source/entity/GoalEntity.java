26fedd8d6e7be9c0d7682dfeac7741c2f4025eed
JSL107 <juneseok0107@naver.com>
2025-08-18 22:33:32 +0900

    feat: merge fix

--- app/src/main/java/com/growit/app/goal/infrastructure/persistence/goal/source/entity/GoalEntity.java
+++ app/src/main/java/com/growit/app/goal/infrastructure/persistence/goal/source/entity/GoalEntity.java
@@ -73,6 +73,7 @@ public class GoalEntity extends BaseEntity {
           goal.getPlans().stream()
               .filter(p -> p.getId() != null)
               .collect(Collectors.toMap(Plan::getId, Plan::getContent, (a, b) -> b));
+
       for (PlanEntity pe : this.plans) {
         String newContent = contentById.get(pe.getUid());
         if (newContent != null && !Objects.equals(pe.getContent(), newContent)) {
