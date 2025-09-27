89d8dba8396613724b612853994cd0ec51767e11
dyno-jun <dyno.jun@gmail.com>
2025-09-28 02:05:37 +0900

    feat: 유효성 검사 추가 및 리트로스펙트 업데이트 로직 개선 (#258)
    - UpdateRetrospectUseCase에서 kpt 필수 여부 검증 추가
    - UpdateRetrospectRequest에 @NotNull 검증 적용
    - RetrospectRequestMapper에서 kpt 객체 생성 로직 개선
    - null 체크 및 예외 처리 강화하여 안정성 향상

--- app/src/main/java/com/growit/app/retrospect/controller/retrospect/mapper/RetrospectRequestMapper.java
+++ app/src/main/java/com/growit/app/retrospect/controller/retrospect/mapper/RetrospectRequestMapper.java
@@ -1,6 +1,7 @@
 package com.growit.app.retrospect.controller.retrospect.mapper;
 
 import com.growit.app.retrospect.controller.retrospect.dto.request.CreateRetrospectRequest;
+import com.growit.app.retrospect.controller.retrospect.dto.request.KPTDto;
 import com.growit.app.retrospect.controller.retrospect.dto.request.UpdateRetrospectRequest;
 import com.growit.app.retrospect.domain.retrospect.dto.*;
 import com.growit.app.retrospect.domain.retrospect.vo.KPT;
@@ -10,28 +11,28 @@ import org.springframework.stereotype.Component;
 public class RetrospectRequestMapper {
 
   public CreateRetrospectCommand toCreateCommand(String userId, CreateRetrospectRequest request) {
-    KPT kpt = null;
-    if (request.getKpt() != null) {
-      kpt =
-          new KPT(
-              request.getKpt().getKeep(),
-              request.getKpt().getProblem(),
-              request.getKpt().getTryNext());
-    } else if (request.getContent() != null) {
-      kpt = new KPT("", "", request.getContent());
-    }
-
+    KPT kpt = getKpt(request.getKpt(), request.getContent());
     return new CreateRetrospectCommand(request.getGoalId(), request.getPlanId(), userId, kpt);
   }
 
   public UpdateRetrospectCommand toUpdateCommand(
       String id, String userId, UpdateRetrospectRequest request) {
-    KPT kpt =
-        new KPT(
-            request.getKpt().getKeep(),
-            request.getKpt().getProblem(),
-            request.getKpt().getTryNext());
+    KPT kpt = getKpt(request.getKpt(), request.getContent());
     return new UpdateRetrospectCommand(id, userId, kpt);
+  }
+
+  private static KPT getKpt(KPTDto request, String request1) {
+    KPT kpt = null;
+    if (request != null) {
+      kpt =
+        new KPT(
+          request.getKeep(),
+          request.getProblem(),
+          request.getTryNext());
+    } else if (request1 != null) {
+      kpt = new KPT("", "", request1);
+    }
+    return kpt;
   }
 
   public GetRetrospectQueryFilter toGetCommand(String id, String userId) {
